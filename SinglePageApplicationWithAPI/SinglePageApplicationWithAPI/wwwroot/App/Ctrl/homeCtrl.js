e04ec4580c6edf12abea1cbc890716c75bdad6de
Mohit Garg <mohit-garg@outlook.com>
2019-02-10 07:26:21 +0000

    SPA With API

---
+++ SinglePageApplicationWithAPI/SinglePageApplicationWithAPI/wwwroot/App/Ctrl/homeCtrl.js
@@ -0,0 +1,65 @@
+﻿(function () {
+
+    // The HTML for this View
+    var viewHTML;
+    // Instantiate the ADAL AuthenticationContext
+    var authContext = new AuthenticationContext(config);
+
+    function registerDataClickHandlers() {
+
+        // View Data Button
+        $("#ViewData").click(function (event) {
+            clearErrorMessage();
+
+            // Acquire Token for Backend
+            authContext.acquireToken(authContext.config.resourceId, function (error, token) {
+
+                // Handle ADAL Errors
+                if (error || !token) {
+                    printErrorMessage('ADAL Error Occurred: ' + error);
+                    return;
+                }
+
+                jQuery.support.cors = true;
+
+                // Get values
+                $.ajax({
+                    type: "GET",
+                    url: "https://localhost.fiddler:44362/api/values",
+                    headers: {
+                        'Authorization': 'Bearer ' + token
+                    }
+                }).done(function (data) {
+                    $("#lblData").text("values returned from API are: " + data[0] + ", " + data[1]);
+                    console.log('Get Call Sucessfull');
+                }).fail(function () {
+                    console.log('Fail to get values');
+                    printErrorMessage('Error in Getting Values');
+                });
+            });
+        });
+    }
+
+    function clearErrorMessage() {
+        var $errorMessage = $(".app-error");
+        $errorMessage.empty();
+    };
+
+    function printErrorMessage(mes) {
+        var $errorMessage = $(".app-error");
+        $errorMessage.html(mes);
+    }
+
+    // Module
+    window.homeCtrl = {
+        requireADLogin: true,
+        preProcess: function (html) {
+
+        },
+        postProcess: function (html) {
+            viewHTML = html;
+            registerDataClickHandlers();
+        }
+    };
+}());
+
---
+++ SinglePageApplicationWithAPI/WebAPI/Extensions/AzureAdServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Extensions
+{
+    public static class AzureAdServiceCollectionExtensions
+    {
+        public static AuthenticationBuilder AddAzureAdBearer(this AuthenticationBuilder builder)
+            => builder.AddAzureAdBearer(_ => { });
+
+        public static AuthenticationBuilder AddAzureAdBearer(this AuthenticationBuilder builder, Action<AzureAdOptions> configureOptions)
+        {
+            builder.Services.Configure(configureOptions);
+            builder.Services.AddSingleton<IConfigureOptions<JwtBearerOptions>, ConfigureAzureOptions>();
+            builder.AddJwtBearer();
+            return builder;
+        }
+
+        private class ConfigureAzureOptions : IConfigureNamedOptions<JwtBearerOptions>
+        {
+            private readonly AzureAdOptions _azureOptions;
+
+            public ConfigureAzureOptions(IOptions<AzureAdOptions> azureOptions)
+            {
+                _azureOptions = azureOptions.Value;
+            }
+
+            public void Configure(string name, JwtBearerOptions options)
+            {
+                options.Audience = _azureOptions.ClientId;
+                options.Authority = $"{_azureOptions.Instance}{_azureOptions.TenantId}";
+            }
+
+            public void Configure(JwtBearerOptions options)
+            {
+                Configure(Options.DefaultName, options);
+            }
+        }
+    }
+}
